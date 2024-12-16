df55e270a8441dd901f3ca37c24a8dfc4471d675
Abanoub-Samy <125883082+CyberBonk@users.noreply.github.com>
2024-12-16 20:44:22 +0200

    latest version just in case

--- InputButton.h
+++ InputButton.h
@@ -3,10 +3,10 @@
 
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
+#include "Lamp_Plug.h"
 
-
-void configure_PD0_as_input_portD(void)
-uint8_t read_PD0(void)
-void TriggerButton(void)
+void configure_PD0_as_input_portD(void);
+uint8_t read_PD0(void);
+void TriggerButton(void);
 
 #endif
