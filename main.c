40948653322e2e0cf756e3297ddc19562c4914e4
Abanoub-Samy <21p0356@eng.asu.edu.eg>
2024-12-19 00:16:21 +0200

    adding functions to main.c

--- main.c
+++ main.c
@@ -53,13 +53,23 @@ void Peripheral_Init(void) {
     //__enable_irq();           // Enable global interrupts
 }
 
+//pins used
+//PF1 for Lamp
+//PF2 for plug
+//PD0 for Plug Button
+//PD1 for Lamp Button
+
+
 int main(void) {
     
     // Initialize peripherals
     Peripheral_Init();
+    InitializePortF_Lamp_Plug_timer_Button(void);   //initialize PortF
+    configure_PD0_PD1_as_input_portD(void);         //Initialize portD
 
     // Main loop - SysTick interrupt will handle temperature checking and buzzer
     while (1) {
-        // The main loop does nothing here since SysTick interrupt will handle the logic
+        TriggerButtonForPlug();
+        TriggerButtonForLamp();
     }
 }
