674ac69d989241ef158d0ae96fb1a37dcccecc4f
Ezzeldine88 <167029855+Ezzeldine88@users.noreply.github.com>
2024-12-20 00:46:42 +0200

    InputButton.h

--- InputButton.h
+++ InputButton.h
@@ -5,13 +5,10 @@
 #include "tm4c123gh6pm.h"
 #include "Lamp_Plug.h"
 #include "DOOR.h"
+
 // Function prototypes for configuring and handling button interrupts
 void configure_PD0_PD1_PD2_as_input_with_interrupt(void);
 void GPIOPortD_Handler(void);  // Interrupt Service Routine for Port D
 
-uint8_t read_PD0(void);
-uint8_t read_PD1(void);
-void TriggerButtonForPlug(void);
-void TriggerButtonForLamp(void);
-
 #endif // INPUTBUTTON_H
+
