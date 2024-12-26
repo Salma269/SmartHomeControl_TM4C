c9572992a7683defa7f01fdd1b0db7b74a06264c
FarahElbosiley <121582539+FarahElbosiley@users.noreply.github.com>
2024-12-26 03:34:54 +0300

    Update InputButton.h

--- InputButton.h
+++ InputButton.h
@@ -6,9 +6,7 @@
 #include "Lamp_Plug.h"
 #include "DOOR.h"
 
-// Function prototypes for configuring and handling button interrupts
-void configure_PD0_PD1_PD2_as_input_with_interrupt(void);
-void GPIOPortD_Handler(void);  // Interrupt Service Routine for Port D
+void GPIOPortD_Handler(void)  // Interrupt Service Routine for Port D
 
 #endif // INPUTBUTTON_H
 
