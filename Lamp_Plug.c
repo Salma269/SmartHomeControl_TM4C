c37930c2a8050e736e838e1eef68503f8ec0bdf5
Salma269 <127438269+Salma269@users.noreply.github.com>
2024-12-26 02:29:41 +0200

    Update Lamp_Plug.c
    Changed PF1 to PC4 and PF2 to PC5

--- Lamp_Plug.c
+++ Lamp_Plug.c
@@ -2,39 +2,32 @@
 #include "tm4c123gh6pm.h"
 #include "Lamp_Plug.h"
 #include "InputButton.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_uart.h"
+#include "inc/hw_sysctl.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/uart.h"
+#include "driverlib/pin_map.h"
 
-// Function to initialize Port F
-void PortF_Init(void) {
-    SYSCTL_RCGCGPIO_R |= 0x20;          // Enable clock for Port F
-    while ((SYSCTL_PRGPIO_R & 0x20) == 0);  // Wait for Port F to be ready
-    
-    GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;     // Unlock GPIO Port F
-    GPIO_PORTF_CR_R |= (1U << 1) | (1U << 2);  // Allow changes to PF1 and PF2
-    GPIO_PORTF_DIR_R |= (1U << 1) | (1U << 2); // Set PF1 and PF2 as outputs
-    GPIO_PORTF_DEN_R |= (1U << 1) | (1U << 2); // Enable digital I/O on PF1 and PF2
-    GPIO_PORTF_DATA_R |= (1U << 1) | (1U << 2); // Initially set PF1 and PF2 HIGH (Lamp and Plug OFF)
+void LampOn() {
+    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_4, 0);  // Set PC4 LOW (relay ON)
 }
 
-
-// Function to set PF1 HIGH, which turns Off the Lamp
-void LampOff(void) {
-    GPIO_PORTF_DATA_R |= 0x02;  // Set PF1 high
+void LampOff() {
+    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_4, GPIO_PIN_4);  // Set PC4 HIGH (relay OFF)
 }
 
-// Function to set PF1 LOW, which turns on the Lamp
-void LampOn(void) {
-    GPIO_PORTF_DATA_R &= ~0x02;  // Set PF1 low
+void PlugOn() {
+    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_5, 0);  // Set PC5 LOW (relay ON)
 }
 
-// Function to turn PF2 ON, which turns off the plug
-void PlugOff(void) {
-    GPIO_PORTF_DATA_R |= 0x04;  // Set PF2 high
+void PlugOff() {
+    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_5, GPIO_PIN_5);  // Set PC5 HIGH (relay OFF)
 }
 
-// Function to turn PF2 OFF, which turns on the Plug
-void PlugOn(void) {
-    GPIO_PORTF_DATA_R &= ~0x04;  // Set PF2 low
-}
 
 // for testing purposes only here and down
 void EnableGreenLED(void) {
@@ -53,4 +46,3 @@ void TurnGreenLEDOn(void) {
 void TurnGreenLEDOff(void) {
     GPIO_PORTF_DATA_R &= ~(1 << 3); // Set PF3 low (turn off green LED)
 }
-
