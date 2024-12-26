8f25d1a067f606914695d9880cf86a04c28ad4de
FarahElbosiley <121582539+FarahElbosiley@users.noreply.github.com>
2024-12-26 03:35:34 +0300

    Remove Test Functions

--- Lamp_Plug.c
+++ Lamp_Plug.c
@@ -27,22 +27,3 @@ void PlugOn() {
 void PlugOff() {
     GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_5, GPIO_PIN_5);  // Set PC5 HIGH (relay OFF)
 }
-
-
-// for testing purposes only here and down
-void EnableGreenLED(void) {
-    SYSCTL_RCGCGPIO_R |= 0x20;       // Enable clock for Port F
-    while ((SYSCTL_PRGPIO_R & 0x20) == 0); // Wait for Port F to be ready
-
-    GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;     // Unlock GPIO Port F
-    GPIO_PORTF_CR_R |= (1 << 3);           // Allow changes to PF3
-    GPIO_PORTF_DIR_R |= (1 << 3);          // Set PF3 as output
-    GPIO_PORTF_DEN_R |= (1 << 3);          // Enable digital function for PF3
-}
-
-void TurnGreenLEDOn(void) {
-    GPIO_PORTF_DATA_R |= (1 << 3); // Set PF3 high (turn on green LED)
-}
-void TurnGreenLEDOff(void) {
-    GPIO_PORTF_DATA_R &= ~(1 << 3); // Set PF3 low (turn off green LED)
-}
